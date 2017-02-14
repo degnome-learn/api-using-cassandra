64c087603837f3bb34c040948dc64f0a4d46d187
Daniel Scholl <degnome@gmail.com>
2017-02-14 16:28:24 -0600

    Added Service Test with first run for Create

--- test/dbClient.spec.js
+++ test/dbClient.spec.js
@@ -1,14 +1,12 @@
 const Test = require('tape');
-const DBClient = require('../lib/DbClient');
+const Client = require('../lib/DbClient');
 const config = require('./config');
 
-Test('DBClient', assert => {
-  const database = new DBClient();
-  database.connect(config.db, (err, db) => {
+Test('Database Client', assert => {
+  const client = new Client(config);
+  client.connect((err, db) => {
     assert.ok(err === null, 'Database should connect without an error');
     assert.end();
-    if (db) {
-      db.close(() => process.exit(0));
-    }
+    if (db) { db.close(() => process.exit(0)); }
   });
 });
---
+++ test/service.spec.js
@@ -0,0 +1,83 @@
+const test = require('tape');
+
+const config = require('./config');
+const seed = require('../lib/seed.json');
+const DbClient = require('../lib/DbClient');
+const Service = require('../lib/service');
+
+const client = new DbClient(config);
+const service = new Service(config);
+
+
+const before = test;
+const after = test;
+let Database, Table;
+
+
+before('before', function (assert) {
+  client.connect((err, db) => {
+    const dropTable = 'truncate test.Item;';
+    db.instance.Item.execute_query(dropTable, null, (err, result) => {
+      Database = db;
+      Table = db.instance.Item;
+      assert.end();
+    });
+  });
+});
+
+
+test('creates an item and returns it', assert => {
+  service.create(seed[0], (err, result) => {
+    console.log(err);
+    assert.ok(err === null, 'No Service Error');
+    assert.ok(result.success, 'Should be success');
+    assert.same('Success', result.message, 'Message should be success');
+    assert.end();
+
+  });
+});
+
+
+after('after', (assert) => {
+  Database.close();
+  assert.end();
+  process.exit(0);
+});
+
+
+// function Before (done) {
+//   const dropTable = 'truncate test.Item;';
+//   const client = new DbClient(config);
+//   client.connect((err, db) => {
+//     db.instance.Item.execute_query(dropTable, null, (err, result) => {
+//       done(db);
+//     });
+//   });
+// }
+
+// function After (db) {
+//   db.close(() => process.exit(0));
+// }
+
+// Before(db => {
+//   Test('Service', assert => {
+
+//   });
+
+//   After(db);
+// });
+
+// Test('Service', assert => {
+
+// });
+
+// Test('Service', TC => {
+//   setup((db) => {
+
+//     TC.test('Test 1', assert => {
+//       assert.ok(true);
+//       assert.end();
+//       close(db);
+//     });
+//   });
+// });
